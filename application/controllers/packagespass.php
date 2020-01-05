0df8909e2a100b996d2e1d2c639daf79ca636c69
tense <tense@aaa.com>
2020-01-06 00:19:51 +0700

    tense06

--- application/controllers/packagespass.php
+++ application/controllers/packagespass.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class packagestour extends CI_Controller {
+class packagespass extends CI_Controller {
     public function index(){
-        $this->load->view('home/packages_tour.html');
+        $this->load->view('home/packages_pass.html');
         //$this->load->view('home/single-blog.html');
     }
 }
